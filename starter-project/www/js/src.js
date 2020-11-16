177cf318f2cabc430f14d064b58583bf74371f69
IsaacAlba <call4mefb@gmail.com>
2020-11-16 21:48:52 +0100

    Primera Clase

--- starter-project/www/js/src.js
+++ starter-project/www/js/src.js
@@ -6,18 +6,83 @@
 /* Comentario de
 varias líneas*/
 
-let contador = 1;
-contador = 2;
+// let contador = 1;
+// contador = 2;
 
-console.log('Hola');
-console.log(contador);
+// console.log('Hola');
+// console.log(contador);
 
-let ivan = 14;
-let Ivan = 16;
+// let ivan = 14;
+// let Ivan = 16;
 
-console.log(ivan);
-console.log(Ivan);
+// console.log(ivan);
+// console.log(Ivan);
 
-console.log('Hola', contador, 55);
-console.warn('Hola');
-console.error('Hola');
+// console.log('Hola', contador, 55);
+// console.warn('Hola');
+// console.error('Hola');
+
+// // Boolean
+// const myTrue = true;
+// const myFalse = false;
+
+// // Null
+// const myNull = null;
+
+// // Undefined
+// let myUndefined = undefined;
+
+// console.log(myTrue, myFalse, myNull, myUndefined);
+
+// // Numbers
+// let myNumber = 0;
+// myNumber = 123123123;
+// myNumber = -50;
+// myNumber = 0.4;
+// myNumber = -15.3;
+// console.log(myNumber);
+
+// myNumber = -Infinity;
+// myNumber = +Infinity;
+
+// myNumber = NaN;
+
+// // Strings
+// let myString = 'String';
+// myString = `String de
+// varias líneas`;
+// console.log(myString);
+
+// const name = 'Ivan';
+// const age = 30;
+// let saludo = `Hola, ${name} tiene ${age} años.`;
+// console.log(saludo);
+// console.log(saludo.length);
+
+// console.log('myTrue es', typeof myTrue, ', myNumber es', typeof myNumber, ' y saludo es', typeof saludo);
+
+let myValue = 10 + 4;
+myValue = 10 - 4;
+myValue = 10 * 4;
+myValue = 10 / 4;
+myValue = 10 % 4;
+
+myValue = 0;
+myValue = myValue + 1;
+myValue++;
+myValue--;
+
+myValue += 10;
+myValue -= 10;
+console.log(myValue);
+
+// Elevar a una potencia
+myValue = 10 ** 4;
+
+// Convertir un string a number con un + delante
+myValue = +'14';
+
+// Asignar el mismo valor a varias variables
+let a, b, c;
+a = b = c = 15;
+console.log(a, b, c);
