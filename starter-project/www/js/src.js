4d51679b28ae2e8dcb37dadfba7e0702ce96d7b7
IsaacAlba <call4mefb@gmail.com>
2020-11-16 20:58:12 +0100

    Primer commit

---
+++ starter-project/www/js/src.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-console */
+'use strict';
+
+// Comentario de una línea
+
+/* Comentario de
+varias líneas*/
+
+let contador = 1;
+contador = 2;
+
+console.log('Hola');
+console.log(contador);
+
+let ivan = 14;
+let Ivan = 16;
+
+console.log(ivan);
+console.log(Ivan);
+
+console.log('Hola', contador, 55);
+console.warn('Hola');
+console.error('Hola');
